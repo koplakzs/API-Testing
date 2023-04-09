06156ba756886ebff26376b3cb89684f477e3ee4
koplakzs <atom.go.76@gmail.com>
2023-04-10 00:19:20 +0700

    penambahan inputan user untuk nama dan job

--- lib/screen/post_screen.dart
+++ lib/screen/post_screen.dart
@@ -1,3 +1,5 @@
+import 'dart:ffi';
+
 import 'package:api_testing/core/post_data_user.dart';
 import 'package:api_testing/models/post_users.dart';
 import 'package:api_testing/widget/button.dart';
@@ -13,6 +15,8 @@ class PostScreen extends StatefulWidget {
 
 class _PostScreenState extends State<PostScreen> {
   PostUser dataPost = PostUser();
+  String? name, job;
+  bool? change = false;
   // late Future PostUser(String name, String job);
 
   // @override
@@ -27,49 +31,116 @@ class _PostScreenState extends State<PostScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-          color: const Color.fromARGB(255, 60, 212, 18),
+          color: const Color.fromARGB(255, 165, 165, 165),
           child: Center(
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                SizedBox(
-                  height: 40,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      const Text('ID : '),
-                      Text(dataPost.id == null ? "None" : '${dataPost.id}')
-                    ],
-                  ),
-                ),
-                SizedBox(
-                    height: 40,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text('Name : '),
-                        Text(
-                            dataPost.name == null ? "None" : '${dataPost.name}')
-                      ],
-                    )),
-                SizedBox(
-                  height: 40,
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text('Job : '),
-                        Text(dataPost.job == null ? "None" : '${dataPost.job}')
-                      ]),
-                ),
+                change != false
+                    ? SizedBox(
+                        child: Column(
+                          children: [
+                            SizedBox(
+                              height: 40,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const Text('ID : '),
+                                  Text(dataPost.id == null
+                                      ? "None"
+                                      : '${dataPost.id}')
+                                ],
+                              ),
+                            ),
+                            SizedBox(
+                                height: 40,
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text('Name : '),
+                                    Text(dataPost.name == null
+                                        ? "None"
+                                        : '${dataPost.name}')
+                                  ],
+                                )),
+                            SizedBox(
+                              height: 40,
+                              child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: [
+                                    const Text('Job : '),
+                                    Text(dataPost.job == null
+                                        ? "None"
+                                        : '${dataPost.job}')
+                                  ]),
+                            ),
+                          ],
+                        ),
+                      )
+                    : SizedBox(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text('Name'),
+                                const SizedBox(
+                                  width: 50,
+                                ),
+                                SizedBox(
+                                  width: 100,
+                                  height: 25,
+                                  child: TextField(
+                                    decoration: const InputDecoration(
+                                        border: OutlineInputBorder()),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        name = value;
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(
+                              height: 30,
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                const Text('Job'),
+                                const SizedBox(
+                                  width: 50,
+                                ),
+                                SizedBox(
+                                  width: 100,
+                                  height: 25,
+                                  child: TextField(
+                                    decoration: const InputDecoration(
+                                        border: OutlineInputBorder()),
+                                    onChanged: (value) {
+                                      setState(() {
+                                        job = value;
+                                      });
+                                    },
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
                 Button(
                     onPressed: () => {
-                          postData('Ridho Alfandi', 'Flutter Dev')
-                              .then((value) {
+                          postData(name!, job!).then((value) {
                             // print(value);
                             dataPost.id = value['id'];
                             dataPost.job = value['job'];
                             dataPost.name = value['name'];
-                            setState(() {});
+                            setState(() {
+                              change = true;
+                            });
                           })
                         },
                     text: 'Post')
